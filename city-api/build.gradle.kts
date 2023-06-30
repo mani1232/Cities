f7f6009ac9ac36d668047a67858d0fe3317edd1b
mani123 <70480001+mani1232@users.noreply.github.com>
2023-06-30 12:34:12 +0200

    Small changes

--- city-api/build.gradle.kts
+++ city-api/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
     // https://mvnrepository.com/artifact/org.mongodb/mongodb-driver-sync
     implementation("org.mongodb:mongodb-driver-sync:4.10.1")
+    // https://mvnrepository.com/artifact/com.electronwill.night-config/json
+    implementation("com.electronwill.night-config:json:3.6.6")
 }
---
+++ city-api/src/main/java/cc/worldmandia/Utils.java
@@ -0,0 +1,4 @@
+package cc.worldmandia;
+
+public class Utils {
+}
