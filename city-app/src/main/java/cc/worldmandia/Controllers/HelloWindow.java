28c8cd0370426ef321b39fa2f6751e123444bd8e
Nikita Kovalov <nik.kovalev1760@gmail.com>
2023-07-01 12:17:42 -0400

    1

---
+++ city-app/src/main/java/cc/worldmandia/Controllers/HelloWindow.java
@@ -0,0 +1,20 @@
+package cc.worldmandia.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class HelloWindow {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
---
+++ city-app/src/main/java/cc/worldmandia/Controllers/MainSelectMenu.java
@@ -0,0 +1,21 @@
+package cc.worldmandia.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class MainSelectMenu {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
+
