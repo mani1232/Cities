edbbb487ec6ac2e34488a2fe91052a17817e415a
mani123 <70480001+mani1232@users.noreply.github.com>
2023-07-04 14:32:29 +0200

    Fix

--- city-api/src/main/java/cc/worldmandia/Utils.java
+++ city-api/src/main/java/cc/worldmandia/Utils.java
@@ -1,8 +1,16 @@
 package cc.worldmandia;
 
+import com.electronwill.nightconfig.core.Config;
+import com.electronwill.nightconfig.core.io.ConfigParser;
+import com.electronwill.nightconfig.json.JsonFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -17,6 +25,24 @@ public class Utils {
 
     public static Logger getLogger(Object loggerClass) {
         return LoggerFactory.getLogger(loggerClass.getClass());
+    }
+
+    public static void main(String[] args) {
+        exampleRequest();
+    }
+
+    public static void exampleRequest() {
+        String name = "Coeur d'Alene".replace(" ", "%20");//.replace("'", "%27");
+        try {
+            ConfigParser<Config> jsonParser = JsonFormat.emptyTolerantInstance().createParser();
+            String json = HttpClient.newHttpClient().sendAsync(HttpRequest.newBuilder().uri(URI.create("https://api.api-ninjas.com/v1/city?name=" + name)).GET().setHeader("accept", "application/json").header("X-Api-Key", "2Pcp9UV3NqYcEaHjX53hjw==GRtzwQ41Dvdbw9Xp").build(), HttpResponse.BodyHandlers.ofString()).get().body();
+            System.out.println(json);
+            Config config = jsonParser.parse(json.replace("[", "").replace("]", ""));
+            System.out.println(config.getOrElse("country", "empty"));
+            System.out.println(config);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
