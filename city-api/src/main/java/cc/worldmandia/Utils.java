ced47c1c2cf189a873e50ebc3a66dff923c246de
mani123 <70480001+mani1232@users.noreply.github.com>
2023-07-05 20:32:01 +0200

    Remove web api

--- city-api/src/main/java/cc/worldmandia/Utils.java
+++ city-api/src/main/java/cc/worldmandia/Utils.java
@@ -1,16 +1,8 @@
 package cc.worldmandia;
 
-import com.electronwill.nightconfig.core.Config;
-import com.electronwill.nightconfig.core.io.ConfigParser;
-import com.electronwill.nightconfig.json.JsonFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
