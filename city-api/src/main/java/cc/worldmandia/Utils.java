ba06495c0edd82e1067bef47c0ce8b64822e5486
mani123 <70480001+mani1232@users.noreply.github.com>
2023-07-05 20:31:54 +0200

    Remove web api

--- city-api/src/main/java/cc/worldmandia/Utils.java
+++ city-api/src/main/java/cc/worldmandia/Utils.java
@@ -26,44 +26,4 @@ public class Utils {
     public static Logger getLogger(Object loggerClass) {
         return LoggerFactory.getLogger(loggerClass.getClass());
     }
-
-    public static void main(String[] args) {
-       // exampleRequest();
-        System.out.println(CheckCityExistanceWithAPI("Coeur d'Alene"));
-    }
-
-    public static void exampleRequest() {
-        String name = "Coeur d'Alene45456".replace(" ", "%20");//.replace("'", "%27");
-        try {
-            ConfigParser<Config> jsonParser = JsonFormat.emptyTolerantInstance().createParser();
-            String json = HttpClient.newHttpClient().sendAsync(HttpRequest.newBuilder().uri(URI.create("https://api.api-ninjas.com/v1/city?name=" + name)).GET().setHeader("accept", "application/json").header("X-Api-Key", "2Pcp9UV3NqYcEaHjX53hjw==GRtzwQ41Dvdbw9Xp").build(), HttpResponse.BodyHandlers.ofString()).get().body();
-            System.out.println(json);
-            Config config = jsonParser.parse(json.replace("[", "").replace("]", ""));
-            System.out.println(config.getOrElse("country", "empty"));
-            System.out.println(config);
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static boolean CheckCityExistanceWithAPI(String cityName) {
-        String name = cityName.replace(" ", "%20");
-        try {
-            ConfigParser<Config> jsonParser = JsonFormat.emptyTolerantInstance().createParser();
-            String json = HttpClient.newHttpClient()
-                    .sendAsync(HttpRequest.newBuilder()
-                            .uri(URI.create("https://api.api-ninjas.com/v1/city?name=" + name))
-                            .GET()
-                            .setHeader("accept", "application/json")
-                            .header("X-Api-Key", "2Pcp9UV3NqYcEaHjX53hjw==GRtzwQ41Dvdbw9Xp")
-                            .build(), HttpResponse.BodyHandlers.ofString())
-                    .get()
-                    .body();
-            Config config = jsonParser.parse(json.replace("[", "").replace("]", ""));
-            return !config.getOrElse("country", "empty").equals("empty");
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
